e3a3d7bc0dfb003fd87a40a335dd9f860dba406e
Oh <Ohsangmin@Ohsangmin>
2017-04-30 22:14:10 +0900

    스마트티비 클래스

---
+++ lab7_201620932/src/lab7/SmartTV.java
@@ -0,0 +1,36 @@
+package lab7;
+
+public class SmartTV extends Product implements Networked {
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double maxSpeed() {
+		// TODO Auto-generated method stub
+		return 1.25;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return SmartTV.class.getSimpleName();
+	}
+	
+	int channel;
+	
+	SmartTV() {}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	};
+	
+
+}
